5deb48c221794097e2f7fa40a1d6899fb0b47faf
sebastien cuthbert <blueton23@gmail.com>
2022-11-16 13:58:09 +0100

    02 finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+10.times do
+  restaurant = Restaurant.create!({ name: Faker::Restaurant.name, address: Faker::Address.full_address,
+                                    phone_number: Faker::PhoneNumber.cell_phone,
+                                    category: %w[chinese italian japanese french belgian].sample })
+  puts "Created #{restaurant.name}"
+end
+
+50.times do
+  Review.create!({ content: Faker::Lorem.sentence, rating: Faker::Number.within(range: 0..5),
+                   restaurant_id: Restaurant.all.sample.id })
+end
+puts 'Finished!'
